866f9d95db4c5833423140e4fb05b775f19229e5
alimpp <hmdmpp@gmail.com>
2023-03-18 18:15:12 -0400

    added

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+const goToHome = () => {
+    const elem = document.getElementById('home')
+    return elem.scrollIntoView()
+}
+
+const goToProducts = () => {
+    const elem = document.getElementById('products')
+    return elem.scrollIntoView()
+}
